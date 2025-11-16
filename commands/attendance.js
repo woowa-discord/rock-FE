ec9670f19770a4b47d39c0ba2e8f40b8bd9dde07
stellalee1210 <yeobinlee1210@gmail.com>
2025-11-16 17:23:51 +0900

    feat(studyTime): 공부시간 관련 슬래쉬 명령어 등록 로직 추가

--- commands/attendance.js
+++ commands/attendance.js
@@ -1,30 +1,30 @@
-import { SlashCommandBuilder } from 'discord.js';
-import { checkAttendance } from './attendanceHandlers/check.js';
-import { getAttendanceStats } from './attendanceHandlers/stats.js';
-import { getAttendanceRanking } from './attendanceHandlers/ranking.js';
+import { SlashCommandBuilder } from "discord.js";
+import { checkAttendance } from "./attendanceHandlers/check.js";
+import { getAttendanceStats } from "./attendanceHandlers/stats.js";
+import { getAttendanceRanking } from "./attendanceHandlers/ranking.js";
 
 export default {
   data: new SlashCommandBuilder()
-    .setName('출석')
-    .setDescription('출석 관련 명령어')
+    .setName("출석")
+    .setDescription("출석 관련 명령어")
     .addSubcommand((subcommand) =>
-      subcommand.setName('체크').setDescription('오늘의 출석을 체크합니다')
+      subcommand.setName("체크").setDescription("오늘의 출석을 체크합니다")
     )
     .addSubcommand((subcommand) =>
-      subcommand.setName('현황').setDescription('출석 현황을 확인합니다')
+      subcommand.setName("현황").setDescription("출석 현황을 확인합니다")
     )
     .addSubcommand((subcommand) =>
-      subcommand.setName('순위').setDescription('출석 순위를 확인합니다.')
+      subcommand.setName("순위").setDescription("출석 순위를 확인합니다.")
     ),
 
   async execute(interaction) {
     const subcommand = interaction.options.getSubcommand();
 
-    if (subcommand === '체크') {
+    if (subcommand === "체크") {
       await checkAttendance(interaction);
-    } else if (subcommand === '현황') {
+    } else if (subcommand === "현황") {
       await getAttendanceStats(interaction);
-    } else if (subcommand === '순위') {
+    } else if (subcommand === "순위") {
       await getAttendanceRanking(interaction);
     }
   },
---
+++ commands/studyTime.js
@@ -0,0 +1,30 @@
+import { SlashCommandBuilder } from "discord.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("공부시간")
+    .setDescription("측정한 공부시간을 확인할 수 있는 명령어")
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("-하루")
+        .setDescription("오늘의 공부시간을 확인합니다.")
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("-일주일")
+        .setDescription("일주일 동안의 공부시간을 확인합니다.")
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("-한달")
+        .setDescription("한 달 간의 공부시간을 확인합니다.")
+    ),
+  async execute(interaction) {
+    const subCommand = interaction.options.getSubcommand();
+
+    if (subCommand === "-하루") {
+    } else if (subCommand === "-일주일") {
+    } else if (subCommand === "-한달") {
+    }
+  },
+};
