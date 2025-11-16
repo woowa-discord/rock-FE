f10abad7edee65e279f6e0f766d95d8e478539bc
stellalee1210 <yeobinlee1210@gmail.com>
2025-11-16 19:11:10 +0900

    fix(studyTime): DB 속성 변경으로 인한 SQL문 수정

--- db/queries/studyTime.js
+++ db/queries/studyTime.js
@@ -1,5 +1,5 @@
 export const STUDY_TIME_QUERIES = Object.freeze({
-  SAVE_STUDY_TIME: `INSERT INTO session (user_id, study_date, start_time, end_time, total_study_time)
+  SAVE_STUDY_TIME: `INSERT INTO session (user_id, study_date, start_time, end_time, study_time)
     VALUES( $1, $2, $3, $4, $5)`,
-  FETCH_DAY_STUDY_TIME: `SELECT total_study_time from session WHERE study_date = $1 ORDER BY total_study_time desc`,
+  FETCH_DAY_STUDY_TIME: `SELECT sum(study_time) as total_study_time from session WHERE study_date = $1`,
 });
