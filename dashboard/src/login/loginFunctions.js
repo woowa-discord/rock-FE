81d5c240c4b0ae960d00394920dae03e5fd6e719
stellalee1210 <yeobinlee1210@gmail.com>
2025-11-22 10:55:37 +0900

    refactor(loginFunctions): 이중 if문 제거 후 try catch 도입

--- dashboard/src/login/loginFunctions.js
+++ dashboard/src/login/loginFunctions.js
@@ -6,24 +6,24 @@ export const code2AccessToken = async (url, hasRequestedAccessToken) => {
 
   //url에 code가 있고 아직 supabase에 요청을 안 했을 때만 실행
   if (code && !hasRequestedAccessToken.current) {
-    hasRequestedAccessToken.current = true;
-    const { data, error } = await supabase.functions.invoke("discord-oauth", {
-      body: { code },
-    });
+    try {
+      hasRequestedAccessToken.current = true;
+      const data = await supabase.functions.invoke("discord-oauth", {
+        body: { code },
+      });
 
-    if (error) {
-      console.error("에러 발생:", error);
-      alert("로그인에 실패했습니다");
-      // 에러가 나면 code는 사용하지 못하기 때문에 삭제
-      window.history.replaceState({}, document.title, "/");
-      return false;
-    } else {
       console.log("로그인 성공!");
       localStorage.setItem("discord_access_token", data.access_token);
 
       // 페이지를 새로고침 하지 않고 url에 code만 지운 상태로 변경
       window.history.replaceState({}, document.title, window.location.pathname);
       return true;
+    } catch (error) {
+      console.error("에러 발생:", error);
+      alert("로그인에 실패했습니다");
+      // 에러가 나면 code는 사용하지 못하기 때문에 삭제
+      window.history.replaceState({}, document.title, "/");
+      return false;
     }
   }
 };
